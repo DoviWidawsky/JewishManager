ea9605c1da42ec51cdc3521ed06e0255243cf42b
Dovi Widawsky <doviwid@gmail.com>
2020-02-19 22:19:17 +0200

    added

--- src/components/screens/NavBar.js
+++ src/components/screens/NavBar.js
@@ -1,37 +1,38 @@
-import React from 'react';
-import { makeStyles } from '@material-ui/core/styles';
-import AppBar from '@material-ui/core/AppBar';
-import Toolbar from '@material-ui/core/Toolbar';
-import Typography from '@material-ui/core/Typography';
-import Button from '@material-ui/core/Button';
-import IconButton from '@material-ui/core/IconButton';
-import MenuIcon from '@material-ui/icons/Menu';
-import Menu from '@material-ui/core/Menu';
-import MenuItem from '@material-ui/core/MenuItem';
-import ReactDOM from 'react-dom';
-import { Route, Link, BrowserRouter as Router } from 'react-router-dom'
-import '../../index.css';
-import App from '../../App';
-import Edit from './Edit'
-import util from '../util'
-import {Report} from '../screens'
-
+import React from "react";
+import { makeStyles } from "@material-ui/core/styles";
+import AppBar from "@material-ui/core/AppBar";
+import Toolbar from "@material-ui/core/Toolbar";
+import Typography from "@material-ui/core/Typography";
+import Button from "@material-ui/core/Button";
+import IconButton from "@material-ui/core/IconButton";
+import MenuIcon from "@material-ui/icons/Menu";
+import Menu from "@material-ui/core/Menu";
+import MenuItem from "@material-ui/core/MenuItem";
+import ReactDOM from "react-dom";
+import { Route, Link, BrowserRouter as Router } from "react-router-dom";
+import "../../index.css";
+import App from "../../App";
+import Edit from "./Edit";
+import util from "../util";
+import { Report } from "../screens";
+import Login from "./Login/login";
 
 const useStyles = makeStyles(theme => ({
   root: {
-    flexGrow: 1,
+    flexGrow: 1
   },
   menuButton: {
-    marginRight: theme.spacing(2),
+    marginRight: theme.spacing(2)
   },
   title: {
-    flexGrow: 1,
-  },
+    flexGrow: 1
+  }
 }));
 
 export default function ButtonAppBar() {
   const classes = useStyles();
   const [anchorEl, setAnchorEl] = React.useState(null);
+  const [isUnlocked, setIsUnlocked] = React.useState(false);
 
   const handleClick = event => {
     setAnchorEl(event.currentTarget);
@@ -41,35 +42,55 @@ export default function ButtonAppBar() {
     setAnchorEl(null);
   };
 
+  const handleLogin = () => {
+    setIsUnlocked(true);
+  };
+
   return (
+    <div>
+      {!isUnlocked && <Login setIsUnlocked={handleLogin} />}
+      {isUnlocked && (
         <Router>
-            <div className={classes.root}>
-                <AppBar position="static">
-                    <Toolbar>
-                        <IconButton onClick={handleClick} edge="start" className={classes.menuButton} color="inherit" aria-label="menu">
-                            <MenuIcon />
-                        </IconButton>
-                        <Menu
-                            id="simple-menu"
-                            anchorEl={anchorEl}
-                            keepMounted
-                            open={Boolean(anchorEl)}
-                            onClose={handleClose}
-                        >
-                        <MenuItem onClick={handleClose}><Link to="/">Home</Link></MenuItem>
-                        <MenuItem onClick={handleClose}><Link to="/report">Reports</Link></MenuItem>
-                        <MenuItem onClick={handleClose}><Link to="/edit">Edit</Link></MenuItem>
-                        </Menu>
-                        <Typography variant="h6" className={classes.title}>
-                            Manager
-                        </Typography>
-                        <Button color="inherit">Login</Button>
-                    </Toolbar>
-                </AppBar>
-                <Route exact path="/" component={App} />
-                <Route path="/report" component={Report} />
-                <Route path="/edit" component={Edit} />
-            </div>
+          <div className={classes.root}>
+            <AppBar position="static">
+              <Toolbar>
+                <IconButton
+                  onClick={handleClick}
+                  edge="start"
+                  className={classes.menuButton}
+                  color="inherit"
+                  aria-label="menu"
+                >
+                  <MenuIcon />
+                </IconButton>
+                <Menu
+                  id="simple-menu"
+                  anchorEl={anchorEl}
+                  keepMounted
+                  open={Boolean(anchorEl)}
+                  onClose={handleClose}
+                >
+                  <MenuItem onClick={handleClose}>
+                    <Link to="/">Home</Link>
+                  </MenuItem>
+                  <MenuItem onClick={handleClose}>
+                    <Link to="/report">Reports</Link>
+                  </MenuItem>
+                  <MenuItem onClick={handleClose}>
+                    <Link to="/edit">Edit</Link>
+                  </MenuItem>
+                </Menu>
+                <Typography variant="h6" className={classes.title}>
+                  Manager
+                </Typography>
+              </Toolbar>
+            </AppBar>
+            <Route exact path="/report" component={Report} />
+            <Route path="/report" component={Report} />
+            <Route path="/edit" component={Edit} />
+          </div>
         </Router>
+      )}
+    </div>
   );
 }
